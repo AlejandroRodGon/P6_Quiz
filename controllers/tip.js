3b9e2f8c03b8eb3cbf4a834d87fc3298de911a5f
AlejandroRodGon <alerogonza@gmail.com>
2018-06-11 22:23:53 +0200

    p81

--- controllers/tip.js
+++ controllers/tip.js
@@ -18,13 +18,25 @@ exports.load = (req, res, next, tipId) => {
 };
 
 
+//Controladores Practica8.
+exports.adminOrAuthorRequired = (req, res, next) => {
+
+    const isAdmin  = !!req.session.user.isAdmin;
+    const isAuthor = req.tip.authorId === req.session.user.id;
+
+    
+};
+
+
+
 // POST /quizzes/:quizId/tips
 exports.create = (req, res, next) => {
- 
+    const authorId = req.session.user && req.session.user.id || 0;
     const tip = models.tip.build(
         {
             text: req.body.text,
-            quizId: req.quiz.id
+            quizId: req.quiz.id,
+            authorId: authorId
         });
 
     tip.save()
@@ -62,6 +74,40 @@ exports.accept = (req, res, next) => {
     });
 };
 
+//Controlador Practica8
+// GET /quizzes/:quizId/tips/:tipId/edit
+exports.edit = (req, res, next) => {
+
+    const {quiz, tip} = req;
+
+    res.render('tips/edit', {quiz, tip});
+};
+
+
+// PUT /quizzes/:quizId/tips/:tipId
+exports.update = (req, res, next) => {
+
+    const {quiz, tip} = req;
+    tip.text = req.body.text;
+    
+    tip.save({fields: ["text", "accepted"]})
+    .then(tip => {
+        req.flash('success', 'Tip edited successfully.');
+        res.redirect('/quizzes/' + quiz.id);
+    })
+    .catch(Sequelize.ValidationError, error => {
+        req.flash('error', 'There are errors in the form:');
+        error.errors.forEach(({message}) => req.flash('error', message));
+        res.render('tips/edit', {quiz, tip});
+    })
+    .catch(error => {
+        req.flash('error', 'Error editing the Tip: ' + error.message);
+        next(error);
+    });
+
+    tip.accepted=false;
+};
+
 
 // DELETE /quizzes/:quizId/tips/:tipId
 exports.destroy = (req, res, next) => {
@@ -73,4 +119,5 @@ exports.destroy = (req, res, next) => {
     })
     .catch(error => next(error));
 };
+
 
